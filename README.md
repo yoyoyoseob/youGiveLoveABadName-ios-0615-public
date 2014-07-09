d799121660152a86c4dfb1454ef068871ee21c49
Joe Burgess <joe@flatironschool.com>
2014-07-09 16:22:18 -0400

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+---
+tags: uianimation, keyframe
+languages: objc
+---
+# You Give Love A Bad Name
+
+Lets learn about `UIKeyFrameAnimations`
+
+## Instructions
+
+This is your goal:
+
+![Animation](animation.gif)
+
+[You'll need UIKeyFrameAnimations](http://www.shinobicontrols.com/blog/posts/2013/10/04/ios7-day-by-day-day-11-uiview-key-frame-animations)
---
+++ youGiveLoveABadName/FISViewController.m
@@ -0,0 +1,33 @@
+//
+//  FISViewController.m
+//  youGiveLoveABadName
+//
+//  Created by Joe Burgess on 7/9/14.
+//  Copyright (c) 2014 Joe Burgess. All rights reserved.
+//
+
+#import "FISViewController.h"
+
+@interface FISViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *bonJovi;
+- (IBAction)expand:(id)sender;
+
+@end
+
+@implementation FISViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)expand:(id)sender {
+}
+@end
