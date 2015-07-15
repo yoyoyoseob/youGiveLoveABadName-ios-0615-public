25d1364d7e520b824058910e5386b8debf2bc8ae
yoyoyoseob <yoyoyoseob@gmail.com>
2015-07-15 17:35:51 -0400

    finished

--- youGiveLoveABadName/FISViewController.m
+++ youGiveLoveABadName/FISViewController.m
@@ -10,6 +10,10 @@
 
 @interface FISViewController ()
 @property (strong, nonatomic) IBOutlet UIImageView *bonJovi;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageHeightConstraint;
+@property (nonatomic) BOOL isExpanded;
+@property (weak, nonatomic) IBOutlet UIButton *expandButton;
+
 - (IBAction)expand:(id)sender;
 
 @end
@@ -20,6 +24,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [[self.expandButton layer] setBorderWidth:1.0f];
+    [[self.expandButton layer] setBorderColor:[UIColor blackColor].CGColor];
+    [[self.expandButton layer] setCornerRadius:4.0f];
 }
 
 - (void)didReceiveMemoryWarning
@@ -28,6 +36,53 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)expand:(id)sender {
+- (IBAction)expand:(id)sender
+{
+    if (!(self.isExpanded))
+    {
+        [UIView animateKeyframesWithDuration:1.0 delay:0.0 options:0 animations:^{
+            
+            [UIView addKeyframeWithRelativeStartTime:0.0 relativeDuration:0.2 animations:^{
+                self.imageHeightConstraint.constant = -10;
+                [self.view layoutIfNeeded];
+            }];
+            
+            [UIView addKeyframeWithRelativeStartTime:0.2 relativeDuration:0.6 animations:^{
+                self.imageHeightConstraint.constant = 395;
+                [self.view layoutIfNeeded];
+            }];
+            
+            [UIView addKeyframeWithRelativeStartTime:0.8 relativeDuration:0.2 animations:^{
+                self.imageHeightConstraint.constant = 385;
+                [self.view layoutIfNeeded];
+            }];
+            
+        } completion:nil];
+    }
+
+    else
+    {
+        [UIView animateKeyframesWithDuration:1.0 delay:0.0 options:0 animations:^{
+            
+            [UIView addKeyframeWithRelativeStartTime:0.0 relativeDuration:0.2 animations:^{
+                self.imageHeightConstraint.constant = 400;
+                [self.view layoutIfNeeded];
+            }];
+            
+            [UIView addKeyframeWithRelativeStartTime:0.2 relativeDuration:0.6 animations:^{
+                self.imageHeightConstraint.constant = -20;
+                [self.view layoutIfNeeded];
+            }];
+            
+            [UIView addKeyframeWithRelativeStartTime:0.8 relativeDuration:0.2 animations:^{
+                self.imageHeightConstraint.constant = 0;
+                [self.view layoutIfNeeded];
+            }];
+            
+        } completion:nil];
+    }
+    
+    self.isExpanded = !self.isExpanded;
 }
+
 @end
